7b47e49641cfe7f21d58502484c57e242811ab6a
gokul12092000 <132733623+gokul12092000@users.noreply.github.com>
2023-05-06 11:01:31 +0530

    car and mobile files got uploaded

---
+++ car.java
@@ -0,0 +1,24 @@
+package week1day1;
+
+public class car{
+	public void applybreak() {
+		System.out.println("applybreak");
+		}
+	public void driveacar() {
+		System.out.println("driveacar");
+		}
+	public void switchonAc() {
+		System.out.println("switchonAc");
+	}
+	public static void main(String[] args) {
+	  car car=new car();
+	  car.applybreak();
+	 car.driveacar();
+		 car.switchonAc();
+	}
+}
+	
+	
+		
+
+
---
+++ mobile.java
@@ -0,0 +1,23 @@
+package week1day1;
+
+public class mobile {
+	public void call() {
+		System.out.println("call");
+		}
+	public void sendmessage() {
+		System.out.println("sendmessage");
+		}
+	public void sharedocument() {
+		System.out.println("sharedocument");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 mobile mobile=new mobile();
+		  mobile.call();
+		 mobile.sendmessage();
+			 mobile.sharedocument();
+
+	}
+
+}
