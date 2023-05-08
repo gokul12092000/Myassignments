9dd8c77222995c20775bbe5bb467ff635e84d71c
gokul12092000 <132733623+gokul12092000@users.noreply.github.com>
2023-05-08 20:31:15 +0530

    bike and mobile files uploaded

---
+++ bike.java
@@ -0,0 +1,24 @@
+package week2day1;
+
+public class bike {
+
+	public static void main(String[] args) {
+		
+		int noOfwheels=2;
+		System.out.println(noOfwheels);
+		short noOfmirrors=2;
+		System.out.println(noOfmirrors);
+		long chassisnumber=234567976l;
+		System.out.println(chassisnumber);
+		boolean ispunctured=true;
+		System.out.println(ispunctured);
+		String bikename="pulsar";
+		System.out.println(bikename);
+		double runningKM=120;
+		System.out.println(runningKM);
+		
+		
+
+	}
+
+}
--- mobile.java
+++ mobile.java
@@ -1,22 +1,26 @@
-package week1day1;
+package week2day1;
 
 public class mobile {
-	public void call() {
-		System.out.println("call");
-		}
-	public void sendmessage() {
-		System.out.println("sendmessage");
-		}
-	public void sharedocument() {
-		System.out.println("sharedocument");
-	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 mobile mobile=new mobile();
-		  mobile.call();
-		 mobile.sendmessage();
-			 mobile.sharedocument();
+		
+		String mobilebrandname="vivo";
+		System.out.println(mobilebrandname);
+		char mobilelogo='v';
+		System.out.println(mobilelogo);
+		short noOfmobilepiece=24;
+		System.out.println(noOfmobilepiece);
+		int modelNumber=13567;
+		System.out.println(modelNumber);
+		long mobileImeinumber=34568782998l;
+		System.out.println(mobileImeinumber);
+		float mobileprice=15000.0f;
+		System.out.println(mobileprice);
+		boolean isdamaged=false;
+		System.out.println(isdamaged);
+		
+		
+		
 
 	}
 
